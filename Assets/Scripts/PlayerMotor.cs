41518cfaa7d31155801e289542b5724ae308ff48
Json-Ram <jasonram778@gmail.com>
2023-04-17 23:40:54 -0700

    add logic for jumping movement

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -9,6 +9,7 @@ public class PlayerMotor : MonoBehaviour
     private bool isGrounded;
     public float speed = 5f;
     public float gravity = -9.8f;
+    public float jumpHeight = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,10 +31,16 @@ public class PlayerMotor : MonoBehaviour
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
         playerVelocity.y += gravity * Time.deltaTime;
         if (isGrounded && playerVelocity.y < 0)
-        {
             playerVelocity.y = -2f;
-        }
         controller.Move(playerVelocity * Time.deltaTime);
         Debug.Log(playerVelocity.y);
+    }
+
+    public void Jump() 
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
     }
 }
