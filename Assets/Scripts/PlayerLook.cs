82cd49313808ebe342797da6a80469c43c2d1d60
Json-Ram <jasonram778@gmail.com>
2023-04-19 00:07:26 -0700

    add logic for looking left, right with camera

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -17,6 +17,11 @@ public class PlayerLook : MonoBehaviour
         // calculate camera rotation for looking up and down
         xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        // apply this to our camera transform
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        // rotate player to look left and right
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+
 
     }
 }
